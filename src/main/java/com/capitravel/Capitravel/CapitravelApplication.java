209f7e2904099791b1436c085fbb362510c52630
Daniel Chacon <dani3l.uebp@gmail.com>
2024-10-21 20:23:58 -0300

    first commit

---
+++ src/main/java/com/capitravel/Capitravel/CapitravelApplication.java
@@ -0,0 +1,13 @@
+package com.capitravel.Capitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitravelApplication.class, args);
+	}
+
+}
