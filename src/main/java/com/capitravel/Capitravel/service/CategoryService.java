2c8dc7bb92a5dba6e32b6fd76e193f8291228f73
Romina Musacchio <musacchioromina@gmail.com>
2024-10-23 12:08:36 -0300

    Adding Category CRUD.

---
+++ src/main/java/com/capitravel/Capitravel/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.capitravel.Capitravel.service;
+
+import com.capitravel.Capitravel.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    Category getCategoryById(Long id);
+    List<Category> getAllCategories();
+    Category createCategory(Category category);
+    Category updateCategory(Long id, Category category);
+    void deleteCategory(Long id);
+}
