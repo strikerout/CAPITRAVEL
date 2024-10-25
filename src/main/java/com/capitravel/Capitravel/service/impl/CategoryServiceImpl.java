0c7330ecbe0eaacdecef887bee924b81bfed91ed
Romina Musacchio <musacchioromina@gmail.com>
2024-10-25 09:03:22 -0300

    Validations for Category added to service level.
    Name: 3<x<32
    Description: 5<x<256

--- src/main/java/com/capitravel/Capitravel/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/capitravel/Capitravel/service/impl/CategoryServiceImpl.java
@@ -21,7 +21,13 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category getCategoryById(Long id) {
-        return categoryRepository.findById(id).orElse(null);
+        Category idCategory = categoryRepository.findById(id).orElse(null);
+
+        if(idCategory!=null){
+            return idCategory;
+        }
+
+        throw new IllegalArgumentException("Category for id:" + id + " not found.");
     }
 
     @Override
@@ -31,24 +37,40 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category createCategory(Category category) {
-        return categoryRepository.save(category);
+
+       if(validateCategory(category)) {
+           return categoryRepository.save(category);
+       }
+        throw new IllegalArgumentException("Category does not follow basic structure.");
     }
 
     @Override
     public Category updateCategory(Long id, Category category) {
         Optional<Category> existingCategory = categoryRepository.findById(id);
 
-        if (existingCategory.isPresent()) {
-            Category updatedCategory = existingCategory.get();
-            updatedCategory.setName(category.getName());
-            updatedCategory.setDescription(category.getDescription());
-            return categoryRepository.save(updatedCategory);
+        if (existingCategory.isPresent() && validateCategory(category)) {
+
+                Category updatedCategory = existingCategory.get();
+                updatedCategory.setName(category.getName());
+                updatedCategory.setDescription(category.getDescription());
+                return categoryRepository.save(updatedCategory);
+
         }
-        return null;
+        throw new IllegalArgumentException("The Category for id:" + id + " not found.");
     }
 
     @Override
     public void deleteCategory(Long id) {
         categoryRepository.deleteById(id);
+    }
+
+    public boolean validateCategory (Category category){
+        if (category.getName() == null || category.getName().isEmpty() || category.getName().length() < 3 || category.getName().length() > 50) {
+            throw new IllegalArgumentException("Category name must be between 3 and 50 characters long.");
+        }
+        else if (category.getDescription() == null || category.getDescription().isEmpty() || category.getDescription().length() < 5 || category.getDescription().length() > 100) {
+            throw new IllegalArgumentException("Category description must be between 5 and 100 characters long.");
+        }
+        return true;
     }
 }
