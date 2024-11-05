d592288b9846ea887f0910c18235647ec0c98aa3
Romina Musacchio <musacchioromina@gmail.com>
2024-11-05 20:52:03 -0300

    Deleting the reference "Product" to clean code for merging to main. Deleting: "Product", "ProductController", "ProductService" and "ProductServiceImpl"

--- src/main/java/com/capitravel/Capitravel/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capitravel.Capitravel.repository;
-
-import com.capitravel.Capitravel.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
